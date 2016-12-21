2be783bab71707a422b5fa8de940a4666554b9c3
Arunpandian <arunpandianm04@gmail.com>
2016-12-21 09:57:45 +0530

    image resolution changed

--- list_all_user_in_campsite_gitterroom/js/script.js
+++ list_all_user_in_campsite_gitterroom/js/script.js
@@ -14,7 +14,7 @@ for($i = 0 ; $i < 10 ; $i++)
       url: 'https://api.gitter.im/v1/rooms/570a5925187bb6f0eadebf05/users?access_token=ae28f23f134c4364ad45e7b7355cfa91c92038bb&limit=100&skip=' + $skip,
       success: function(data){
         $.each(data, function(i, list){
-          $myList.append('<li><table><tr><td><img src = "' + list.avatarUrlSmall + '" alt = "avatar or profile picture of Gitter user" width=128 height=128></td><td><td><p class = "left-align" >User ID : </p><p class = "left-align" >User Name : </p><p class = "left-align" >Role : </p></td><td><p>' + list.id + '</p><p>' + list.username + '</p><p>' + list.role + '</p></td></td></tr></table><hr></li>');
+          $myList.append('<li><table><tr><td><img src = "' + list.avatarUrl + '" alt = "avatar or profile picture of Gitter user" width=128 height=128></td><td><td><p class = "left-align" >User ID : </p><p class = "left-align" >User Name : </p><p class = "left-align" >Role : </p></td><td><p>' + list.id + '</p><p>' + list.username + '</p><p>' + list.role + '</p></td></td></tr></table><hr></li>');
       });
       }
       });
